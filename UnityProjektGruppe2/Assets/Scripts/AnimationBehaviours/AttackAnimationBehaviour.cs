4847f2fe135150fb091b1f7553646e2aa32ba44b
Niclas 'Fashdey' Andersen <Fashdey@outlook.com>
2015-11-09 15:50:32 +0100

    Whirlwind working! Rotation bug FIXED!

--- UnityProjektGruppe2/Assets/Scripts/AnimationBehaviours/AttackAnimationBehaviour.cs
+++ UnityProjektGruppe2/Assets/Scripts/AnimationBehaviours/AttackAnimationBehaviour.cs
@@ -21,7 +21,7 @@ public class AttackAnimationBehaviour : StateMachineBehaviour {
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         myBoxCollider.enabled = false;
-        animator.SetBool("testAttack", false);
+        animator.SetBool("dAttackSecurity", false);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
--- UnityProjektGruppe2/Assets/Scripts/Player/PlayerController.cs
+++ UnityProjektGruppe2/Assets/Scripts/Player/PlayerController.cs
@@ -6,13 +6,23 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveForce = 5, boostMultiplier = 2;
-    bool isAttacking = false;
+    bool attackBtnPressed = false;
+    bool whirlwindBtnPressed = false;
+    bool whirlwinding = false;
 
     Rigidbody myRigidBody;
     private Animator myAnimator;
     [SerializeField]
     public BoxCollider mySwordBoxCollider;
     float currentSpeed = 0.0f;
+
+
+    //Whirlwind Variables
+    float rotationleft = 0;
+    float rotationspeed = 720; //rotation speed
+    Vector3 angleBeforeWhirlwind;
+    float whirlwindRange = 0.2f;
+    float saveY;
 
     // Use this for initialization
     void Start()
@@ -30,7 +40,10 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Movement();
+        if (!whirlwinding)
+        {
+            Movement();
+        }
         Attack();
     }
 
@@ -42,8 +55,6 @@ public class PlayerController : MonoBehaviour
         float joyX = CrossPlatformInputManager.GetAxis("Horizontal");
         float joyY = CrossPlatformInputManager.GetAxis("Vertical");
 
-        
-
         Vector3 moveVec = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0, CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
         ////bool isBoosting = CrossPlatformInputManager.GetButton("LeButton");
         //bool isBoosting = false;
@@ -52,7 +63,7 @@ public class PlayerController : MonoBehaviour
         if (moveVec.x != 0 && moveVec.z != 0)
         {
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(-joyX, -joyY) * Mathf.Rad2Deg, transform.eulerAngles.z);
-            
+
             //myRigidBody.transform.Rotate(0, moveVec.x * 25 * Time.deltaTime, 0);
         }
 
@@ -81,27 +92,57 @@ public class PlayerController : MonoBehaviour
             }
             myAnimator.SetFloat("Speed", currentSpeed);
         }
-        //
-        //myAnimator.SetFloat("Speed", moveVec.z * moveForce);
-
-        //Debug.Log(moveVec);
-        //Debug.Log(isBoosting.ToString());
-        //Debug.Log(transform.position);
-
-        //myRigidBody.AddForce(moveVec * (isBoosting ? boostMultiplier : 1));
     }
 
     void Attack()
     {
-        isAttacking = CrossPlatformInputManager.GetButton("LeButton");
-        
-        if (isAttacking)
+        attackBtnPressed = CrossPlatformInputManager.GetButtonDown("LeButton");
+
+        if (attackBtnPressed)
         {
-            myAnimator.SetBool("testAttack", true);
-            isAttacking = false;
+            myAnimator.SetBool("dAttackSecurity", true);
+            attackBtnPressed = false;
             myAnimator.SetTrigger("Attack");
         }
-        
+
+        whirlwindBtnPressed = CrossPlatformInputManager.GetButtonDown("Whirlwind");
+        if (whirlwindBtnPressed && !whirlwinding)
+        {
+            whirlwinding = true;
+            angleBeforeWhirlwind = Vector3.Normalize(transform.forward);
+            saveY =  transform.eulerAngles.y;
+
+            rotationleft = 720;
+            myAnimator.SetBool("Whirlwind", true);
+        }
+
+        if (whirlwinding)
+        {
+            float rotation = rotationspeed * Time.deltaTime;
+            bool rotateMe = false;
+            if (rotationleft > rotation)
+            {
+                rotationleft -= rotation;
+                rotateMe = true;
+            }
+            else
+            {
+                rotateMe = false;
+                rotation = rotationleft;
+                rotationleft = 0;
+                whirlwinding = false;
+                myAnimator.SetBool("Whirlwind", false);
+                transform.rotation = Quaternion.Euler(transform.rotation.x, saveY, transform.rotation.z);
+            }
+            if (rotateMe)
+            {
+                transform.parent.Translate(angleBeforeWhirlwind * whirlwindRange);   
+                transform.Rotate(0, rotation, 0);
+                
+            }
+
+        }
+
     }
 
 
